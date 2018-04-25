5067517470f08118ea959b2d0da2c7914d74f7b5
vdhulappanavar <vdhulappanavar@gmail.com>
2018-04-25 18:04:24 +0530

    http-server added

--- server/index.js
+++ server/index.js
@@ -1,2 +1,10 @@
 const express = require('express')
-app.use('/', express.static('../src/index.html'))
+const app = express()
+app.get('/', (req, res) => res.send('Hello  !'))
+app.use('/check', express.static('build'))
+// app.get('/', function(req, res) {
+//   res.sendFile('../src/index.html', {root: __dirname })
+//   res.sendFile('/uploads/' + uid + '/' + file);
+
+// });
+app.listen(38080, () => console.log('Example app listening on port 5000!'))
